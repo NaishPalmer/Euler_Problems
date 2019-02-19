ff9e3423d5143d089c79db47d0e762a410331f61
Dexzify <dexzify@gmail.com>
2019-02-19 22:12:03 +0000

    Added solution to Problem 2.

--- Problem 2/Problem 2.py
+++ Problem 2/Problem 2.py
@@ -1,1 +1,15 @@
 #Problem 2 of Project Euler 
+n1 = 0
+n2 = 1
+nth = n1 + n2
+sum_of_even_nths = 0
+while nth <= 4000000:
+    nth = n1 + n2
+    n1 = n2
+    n2 = nth
+    if nth % 2 == 0:
+        sum_of_even_nths += nth
+        
+        print(sum_of_even_nths)
+
+    
